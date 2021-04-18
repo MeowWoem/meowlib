2a44eb9a217fb06d445456fab91d03feedbcb9a2
Woem <l.stoller@outlook.fr>
2021-04-18 12:31:59 +0200

    Add function that allow to copy tables

--- media/lua/shared/MeowCore.lua
+++ media/lua/shared/MeowCore.lua
@@ -94,3 +94,23 @@ end
 function Dump(o)
 	print(_dump(o));
 end
+
+function DeepCopy(obj)
+    if type(obj) ~= 'table' then return obj end
+    local res = setmetatable({}, getmetatable(obj))
+    for k, v in pairs(obj) do res[DeepCopy(k)] = DeepCopy(v) end
+    return res
+end
+
+function DeepCopyRecursive(obj, seen)
+    -- Handle non-tables and previously-seen tables.
+    if type(obj) ~= 'table' then return obj end
+    if seen and seen[obj] then return seen[obj] end
+
+    -- New table; mark it as seen and copy recursively.
+    local s = seen or {}
+    local res = {}
+    s[obj] = res
+    for k, v in pairs(obj) do res[DeepCopyRecursive(k, s)] = DeepCopyRecursive(v, s) end
+    return setmetatable(res, getmetatable(obj))
+end
