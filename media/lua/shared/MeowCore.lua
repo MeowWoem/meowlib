836e12f2c5df0c9db379f6c552e1e9256de7cb55
Woem <l.stoller@outlook.fr>
2021-04-18 10:24:55 +0200

    Enhance the Dump function

--- media/lua/shared/MeowCore.lua
+++ media/lua/shared/MeowCore.lua
@@ -66,15 +66,19 @@ function MeowCore:require(str)
 end
 
 
+local function _dump(o)
+	if type(o) == 'table' then
+		local s = '{ \n';
+		for k,v in pairs(o) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			s = s .. '['..k..'] = ' .. _dump(v) .. ',';
+		end
+		return (s .. '\n} ');
+	else
+		return (tostring(o));
+	end
+end
+
 function Dump(o)
-   if type(o) == 'table' then
-      local s = '{ \n';
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ',';
-      end
-      print(s .. '\n} ');
-   else
-      print(tostring(o));
-   end
+	print(_dump(o));
 end
