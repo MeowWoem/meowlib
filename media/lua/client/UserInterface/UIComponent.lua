18fc9709daf95df587f16f1fd56bbf2ec2c41158
Woem <l.stoller@outlook.fr>
2021-04-18 12:33:47 +0200

    Use UIStyle to render component

--- media/lua/client/UserInterface/UIComponent.lua
+++ media/lua/client/UserInterface/UIComponent.lua
@@ -3,6 +3,8 @@ require "ISUI/ISUIElement"
 
 MeowCore:namespace("Client/UserInterface");
 
+local UIStyle = MeowCore:require("Client/UserInterface/UIStyle");
+
 local Parent = ISUIElement;
 local UIComponent = Parent:derive("UIComponent");
 
@@ -18,11 +20,17 @@ function UIComponent:new(x, y, width, height)
 	o.y = y;
     o.width = width;
 	o.height = height;
+	o.style = UIStyle:new();
 	return o;
 end
 
 function UIComponent:prerender()
-	self:drawText("You must override UIComponent prerender method!", 0, 0, 0.8, 0, 0, 1, UIFont.Medium);
+	--self:drawText("You must override UIComponent prerender method!", 0, 0, 0.8, 0, 0, 1, UIFont.Medium);
+	local style = self.style;
+	if style.background then
+		self:drawRectStatic(0, 0, self.width, self.height, style.background.a, style.background.r, style.background.g, style.background.b);
+		--self:drawRectBorderStatic(0, 0, self.width, self.height, self.borderColor.a, self.borderColor.r, self.borderColor.g, self.borderColor.b);
+	end
 end
 
 MeowCore.Client.UserInterface.UIComponent = UIComponent;
