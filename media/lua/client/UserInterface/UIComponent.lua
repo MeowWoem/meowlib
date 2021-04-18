351103811c9325a326bed6cdda019dc264fe3726
Woem <l.stoller@outlook.fr>
2021-04-18 18:16:24 +0200

    Adding comments

--- media/lua/client/UserInterface/UIComponent.lua
+++ media/lua/client/UserInterface/UIComponent.lua
@@ -30,63 +30,57 @@ end
 
 function UIComponent:prerender()
 	local style = self.style;
-
 	local bgOffsetW = 0; local bgOffsetH = 0; local bgOffsetX = 0; local bgOffsetY = 0;
 	local borderOffsetW = 0; local borderOffsetH = 0; local borderOffsetX = 0; local borderOffsetY = 0;
-
+	-- # 	PREPARE
+	-- ## 	Prepare background offset
+	-- #### Borders
 	if style.border then
-
 		if(style.border.all or style.border.top) then
 			local b = style.border.all or style.border.top;
 			bgOffsetY = bgOffsetY + b.width;
 			bgOffsetH = bgOffsetH - b.width;
 		end
-
 		if(style.border.all or style.border.left) then
 			local b = style.border.all or style.border.left;
 			bgOffsetX = bgOffsetX + b.width;
 			bgOffsetW = bgOffsetW - b.width;
 		end
-
 		if(style.border.all or style.border.bottom) then
 			local b = style.border.all or style.border.bottom;
 			bgOffsetH = bgOffsetH - b.width;
 		end
-
 		if(style.border.all or style.border.right) then
 			local b = style.border.all or style.border.right;
 			bgOffsetW = bgOffsetW - b.width;
 		end
-
 	end
 
+	-- # 	DRAWING
+	-- #	Background
 	if style.background then
 		self:drawRectStatic(bgOffsetX, bgOffsetY, self.width + bgOffsetW, self.height + bgOffsetH, style.background.a, style.background.r, style.background.g, style.background.b);
 	end
 
 	if style.border then
-
 		if(style.border.all or style.border.top) then
 			local b = style.border.all or style.border.top;
 			if(b.width > 0) then
 				self:drawRectStatic(0, 0, self.width, b.width, b.color.a, b.color.r, b.color.g, b.color.b);
 			end
 		end
-
 		if(style.border.all or style.border.left) then
 			local b = style.border.all or style.border.left;
 			if(b.width > 0) then
 				self:drawRectStatic(0, bgOffsetY, b.width, self.height + bgOffsetH, b.color.a, b.color.r, b.color.g, b.color.b);
 			end
 		end
-
 		if(style.border.all or style.border.bottom) then
 			local b = style.border.all or style.border.bottom;
 			if(b.width > 0) then
 				self:drawRectStatic(0, self.height - b.width, self.width, b.width, b.color.a, b.color.r, b.color.g, b.color.b);
 			end
 		end
-
 		if(style.border.all or style.border.right) then
 			local b = style.border.all or style.border.right;
 			if(b.width > 0) then
@@ -95,7 +89,6 @@ function UIComponent:prerender()
 					local bTop = style.border.all or style.border.top;
 					y = bTop.width;
 				end
-
 				self:drawRectStatic(self.width - b.width, y, b.width, self.height + bgOffsetH, b.color.a, b.color.r, b.color.g, b.color.b);
 			end
 		end
