45ef2e3ae9ef7a98dae7982e7adf0979f65252c1
Woem <l.stoller@outlook.fr>
2021-04-18 18:17:15 +0200

    Update DebugClient.lua

--- media/lua/client/DebugClient.lua
+++ media/lua/client/DebugClient.lua
@@ -2,7 +2,12 @@ require "MeowCore";
 
 local UIPanel = MeowCore:require("Client/UserInterface/UIPanel");
 
-local elm = UIPanel:new(500, 250, 500, 500);
-elm:initialise();
-elm:addToUIManager();
-elm:setVisible(true);
+function test()
+
+	local elm = UIPanel:new(500, 250, 500, 500);
+	elm:initialise();
+	elm:addToUIManager();
+	elm:setVisible(true);
+
+end
+Events.OnGameStart.Add(test);
