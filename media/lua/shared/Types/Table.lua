bb3b6f298ff3d2d55e3975d3889cc428431faabf
Woem <l.stoller@outlook.fr>
2021-04-19 15:32:08 +0200

    Add isEmpty method

--- media/lua/shared/Types/Table.lua
+++ media/lua/shared/Types/Table.lua
@@ -1,3 +1,11 @@
 function table.pack(...)
     return { n = select("#", ...); ... }
 end
+
+function table:isEmpty(o)
+  if(o == nil) then o = self; end
+	for _,_ in pairs(o) do
+		return false
+	end
+	return true
+end
