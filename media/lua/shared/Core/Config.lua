262e696e4733d27177d9fcdb649eae09df3e6796
Woem <l.stoller@outlook.fr>
2021-04-19 18:56:36 +0200

    Move INI functions to a new class INIUtils

--- media/lua/shared/Core/Config.lua
+++ media/lua/shared/Core/Config.lua
@@ -1,6 +1,7 @@
 require "MeowCore";
 
 MeowCore:namespace("Shared/Core");
+local INIUtils = MeowCore:require("Shared/Utils/INIUtils");
 
 local __FILE_SEP__ = getFileSeparator();
 local Config = {};
@@ -39,56 +40,6 @@ local function toINI(modid, filename, content, fd, parentCat, fdTypeMap)
   fdTypeMap:close();
 end
 
-local function fromINI(modid, filename)
-	local retVal = {};
-	local retTypes = {};
-	local rvptr = retVal;
-	local f = getFileReader(modid .. __FILE_SEP__ .. filename, false);
-	local f2 = getModFileReader(modid, "media".. __FILE_SEP__ ..".typemaps" .. __FILE_SEP__ .. filename .. ".typemap", false);
-	if not f or not f2 then return retVal end;
-
-	local line = "1";
-	local line2 = "1";
-	local currentCat = "unknown";
-
-	while line2 do
-		line2 = f2:readLine();
-		if line2 then
-			if luautils.stringStarts(line2, "[") then
-				currentCat = string.match(line2, "[a-zA-Z0-9/]+");
-			else
-				local kv = string.split(line2, "=");
-				retTypes[currentCat .. "/" .. kv[1]] = kv[2];
-			end
-		end
-	end
-
-	while line do
-		line = f:readLine();
-		if line then
-			if luautils.stringStarts(line, "[") then
-				currentCat = string.match(line, "[a-zA-Z0-9/]+");
-				rvptr = retVal;
-				for _, cat in ipairs(string.split(currentCat, "/")) do
-					if not rvptr[cat] then rvptr[cat] = {} end
-					rvptr = rvptr[cat];
-				end
-			else
-				local kv = string.split(line, "=");
-        local vtype = retTypes[currentCat .. "/" .. kv[1]];
-        local v = kv[2];
-        if(vtype == "number") then
-          v = tonumber(v);
-        elseif(vtype == "boolean") then
-          v = v == "true";
-        end
-
-				rvptr[kv[1]] = v;
-			end
-		end
-	end
-	return retVal;
-end
 
 function Config:new(modid, defaultConfig)
 	local o = {};
@@ -104,7 +55,7 @@ function Config:new(modid, defaultConfig)
 end
 
 function Config:load()
-	local cfg = fromINI(self.modid, self.filename);
+	local cfg = INIUtils.INIToTable(self.modid, self.filename);
 	if(table.isEmpty(cfg)) then
 		cfg = {};
 	end
@@ -113,7 +64,7 @@ end
 
 function Config:save()
 	if(self.cfg == nil) then return false end
-	toINI(self.modid, self.filename, self.cfg);
+	INIUtils.TableToINI(self.modid, self.filename, self.cfg);
 	return true;
 end
 
---
+++ media/lua/shared/Utils/INIUtils.lua
@@ -0,0 +1,93 @@
+require "MeowCore";
+
+MeowCore:namespace("Shared/Utils");
+
+local __FILE_SEP__ = getFileSeparator();
+local INIUtils = {};
+
+function INIUtils.TableToINI(modid, filename, content, fd, parentCat, fdTypeMap)
+	if(fd == nil) then
+		fd = getFileWriter(modid .. __FILE_SEP__ .. filename, true, false);
+	end
+	if(fdTypeMap == nil) then
+		fdTypeMap = getFileWriter(modid .. __FILE_SEP__ .. filename .. ".typemap", true, false);
+	end
+
+  if not fd then return false end;
+  local category = "";
+	for catK, catV in pairs(content) do
+		if parentCat then
+      category = parentCat.."/"..catK;
+		else
+			category = catK;
+		end
+		fd:write("["..category.."]\n");
+		fdTypeMap:write("["..category.."]\n");
+
+		for k,v in pairs(catV) do
+			if type(v) == "table" then
+				local categoryTable = {};
+				categoryTable[k] = v;
+				INIUtils.TableToINI(modid, filename, categoryTable, fd, category, fdTypeMap);
+			else
+				fd:write(tostring(k).."="..tostring(v).."\n");
+				fdTypeMap:write(tostring(k).."="..type(v).."\n");
+			end
+		end
+	end
+  fd:close();
+  fdTypeMap:close();
+end
+
+function INIUtils.INIToTable(modid, filename)
+	local retVal = {};
+	local retTypes = {};
+	local rvptr = retVal;
+	local f = getFileReader(modid .. __FILE_SEP__ .. filename, false);
+	local f2 = getModFileReader(modid, "media".. __FILE_SEP__ ..".typemaps" .. __FILE_SEP__ .. filename .. ".typemap", false);
+	if not f or not f2 then return retVal end;
+
+	local line = "1";
+	local line2 = "1";
+	local currentCat = "unknown";
+
+	while line2 do
+		line2 = f2:readLine();
+		if line2 then
+			if luautils.stringStarts(line2, "[") then
+				currentCat = string.match(line2, "[a-zA-Z0-9/]+");
+			else
+				local kv = string.split(line2, "=");
+				retTypes[currentCat .. "/" .. kv[1]] = kv[2];
+			end
+		end
+	end
+
+	while line do
+		line = f:readLine();
+		if line then
+			if luautils.stringStarts(line, "[") then
+				currentCat = string.match(line, "[a-zA-Z0-9/]+");
+				rvptr = retVal;
+				for _, cat in ipairs(string.split(currentCat, "/")) do
+					if not rvptr[cat] then rvptr[cat] = {} end
+					rvptr = rvptr[cat];
+				end
+			else
+				local kv = string.split(line, "=");
+        local vtype = retTypes[currentCat .. "/" .. kv[1]];
+        local v = kv[2];
+        if(vtype == "number") then
+          v = tonumber(v);
+        elseif(vtype == "boolean") then
+          v = v == "true";
+        end
+
+				rvptr[kv[1]] = v;
+			end
+		end
+	end
+	return retVal;
+end
+
+MeowCore.Shared.Utils.INIUtils = INIUtils;
