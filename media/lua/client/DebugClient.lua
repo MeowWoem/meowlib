bad70285c3d5bb141f3babe83ed343d2f542e3ed
Woem <l.stoller@outlook.fr>
2021-04-19 13:55:22 +0200

    Test Buttons

--- media/lua/client/DebugClient.lua
+++ media/lua/client/DebugClient.lua
@@ -1,13 +1,25 @@
 require "MeowCore";
 
 local UIPanel = MeowCore:require("Client/UserInterface/UIPanel");
+local UIComponent = MeowCore:require("Client/UserInterface/UIComponent");
+local UIButton = MeowCore:require("Client/UserInterface/UIButton");
 
 function test()
 
-	local elm = UIPanel:new(500, 250, 500, 500);
+	local elm = UIPanel:new({
+		x = 500,
+		y = 250,
+		width = 500,
+		height = 500
+	});
 	elm:initialise();
 	elm:addToUIManager();
 	elm:setVisible(true);
 
+	local btn = UIButton:new({
+		title = "Button"
+	});
+	elm:addChild(btn);
 end
-Events.OnGameStart.Add(test);
+
+Events.OnMainMenuEnter.Add(test);
