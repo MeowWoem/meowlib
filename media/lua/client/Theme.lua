027edc8ffeb12d7c66892a7226820ca7462ce9b9
Woem <l.stoller@outlook.fr>
2021-04-26 08:56:16 +0200

    Add theme for new elements

--- media/lua/client/Theme.lua
+++ media/lua/client/Theme.lua
@@ -17,6 +17,54 @@ local Theme = {
 		},
 		hover = {}
 	}),
+	UITooltip = Style:new({
+		background = Color:new(0, 0, 0, 0.3),
+		color = Color.white(),
+		font = UIFont.Small,
+		border = {
+			all = {
+				width = 2,
+				color = Color:new(1, 1, 1, 0.1)
+			}
+		},
+		hover = {}
+	}),
+	UIRichTextPanel = Style:new({
+		background = Color.transparent(),
+		color = Color.white(),
+		font = UIFont.Small,
+		border = {
+			all = {
+				width = 0,
+				color = Color.transparent()
+			}
+		},
+		margin = {
+			top=10,
+			right=10,
+			bottom=10,
+			left=20,
+		},
+		hover = {},
+		richText = {
+			H1 = {
+				color = Color.white(),
+				font = UIFont.Large
+			},
+			H2 = {
+				color = Color.lightgray(),
+				font = UIFont.Medium
+			},
+			TEXT = {
+				color = Color.white(),
+				font = UIFont.Small
+			},
+			META = {
+				color = Color.silver(),
+				font = UIFont.Small
+			},
+		}
+	}),
 	UIButton = Style:new({
 		background = Color.black(),
 		color = Color.white(),
@@ -29,6 +77,23 @@ local Theme = {
 		},
 		hover = {
 			background = Color:new(0.3, 0.3, 0.3)
+		},
+		active = {
+			background = Color:new(0.15, 0.15, 0.15)
+		}
+	}),
+	UIResizeWidget = Style:new({
+		background = Color.red(),
+		color = Color.white(),
+		font = UIFont.Small,
+		border = {
+			all = {
+				width = 1,
+				color = Color:new(0.7, 0.7, 0.7, 1)
+			}
+		},
+		hover = {
+			background = Color:new(0, 1, 0.3)
 		},
 		active = {
 			background = Color:new(0.15, 0.15, 0.15)
