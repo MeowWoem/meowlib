22794c3f0cc5872779863dae18dcfc7d366091d5
Woem <l.stoller@outlook.fr>
2021-04-26 08:56:45 +0200

    Add constructors to Actor

--- media/lua/shared/Actors/Actor.lua
+++ media/lua/shared/Actors/Actor.lua
@@ -2,6 +2,23 @@ require "MeowCore";
 
 
 local Actor = MeowCore.class(
-	"Shared/Actors/Actor",
-	{  }
+	"Shared/Actors/Actor", {
+		isoObj = nil
+	}
 );
+
+function Actor:constructor_IsoGameCharacter(iso)
+	self.isoObj = iso;
+end
+
+function Actor:constructor_IsoLivingCharacter(iso)
+	self:constructor_IsoGameCharacter(iso);
+end
+
+function Actor:constructor_IsoPlayer(iso)
+	self:constructor_IsoGameCharacter(iso);
+end
+
+function Actor:constructor_IsoZombie(iso)
+	self:constructor_IsoGameCharacter(iso);
+end
