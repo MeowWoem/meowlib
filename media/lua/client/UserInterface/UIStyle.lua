81e659a9a0acb586d91f8c888733ecb425962f30
Woem <l.stoller@outlook.fr>
2021-04-26 08:54:08 +0200

    Add margins

--- media/lua/client/UserInterface/UIStyle.lua
+++ media/lua/client/UserInterface/UIStyle.lua
@@ -6,16 +6,22 @@ local Color = MeowCore.require("Shared/Types/Color");
 local UIStyle = MeowCore.class(
 	"Client/UserInterface/UIStyle", {
 		background = Color.transparent(),
+		color = Color.white(),
+		font = UIFont.Small,
+		boxSizing = "border-box",
+		textureColor = Color.white(),
 		border = {
 			all = {
 				width = 0,
 				color = Color.transparent()
 			}
 		},
-		color = Color.white(),
-		font = UIFont.Small,
-		boxSizing = "border-box",
-		textureColor = Color.white(),
+		margin = {
+			top=0,
+			right=0,
+			bottom=0,
+			left=0,
+		},
 		hover = {},
 		active = {}
 	}
