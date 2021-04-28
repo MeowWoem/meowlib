a8014e0550d045d6d9d7b56cd620c88a91ab370a
Woem <l.stoller@outlook.fr>
2021-04-28 16:35:45 +0200

    Update MeowServer.lua

--- media/lua/server/MeowServer.lua
+++ media/lua/server/MeowServer.lua
@@ -1,3 +1,19 @@
 require "MeowCore";
 
-MeowServer = {};
+local ActorsManager = MeowCore.require('Server/Managers/ActorsManager');
+
+local MeowServer = MeowCore.class("Server/MeowServer");
+
+MeowServer.actorManager = ActorsManager:new();
+
+function MeowServer.getModData()
+	local md;
+	if(isServer() == true) then -- Not used in B41 IWBMS.
+		md = getServerModData();
+	else
+		md = getSpecificPlayer(0):getModData();
+		md.__meowServer = md.__meowServer or {};
+		md = md.__meowServer;
+	end
+	return md;
+end
