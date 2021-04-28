07dadf3aa3ef15454ba2f59b9f774ca0c9071e14
Woem <l.stoller@outlook.fr>
2021-04-28 10:29:50 +0200

    Change Typed related functions to Typed class

--- media/lua/shared/MeowCore.lua
+++ media/lua/shared/MeowCore.lua
@@ -2,6 +2,11 @@ require "Types/String";
 require "Types/Table";
 require "Math/Math";
 
+local Typed = require("Types/Typed");
+
+local isctype, typed, ccast, cinstanceof, ctype, getTableTypeSignature
+	= Typed:get("isctype, typed, ccast, cinstanceof, ctype, getTableTypeSignature");
+
 
 local function __switchMissing() end
 
@@ -10,95 +15,25 @@ local function switch(value, cases)
 	return case(value);
 end
 
-function isctype(t, value, strict)
-	strict = strict or false;
-	if(t == 'number' and strict) then error("You can't use number type with strict comparaison"); end
-	local tv = ctype(value);
-	return (
-		tv == t or (
-			(t == 'number' or t == 'float') and
-			((strict == false and tv == 'integer') or tv == 'float')
-		)
-	);
-end
-
-function typed(t, value, strict)
-	strict = strict or false;
-	if(isctype(t, value, strict)) then
-		return value;
-	else
-		error("Expected " .. (strict and 'strict ' or '') .. t .. " but " .. ctype(value) .. " given!")
-	end
-end
-
-function ccast(cast, obj)
-	if(type(obj) == "table" and cinstanceof(cast, getmetatable(obj))) then
-		return cast:new(obj);
-	end
-end
-
-function cinstanceof (subject, super)
-	super = tostring(super);
-	local mt = getmetatable(subject);
-	while true do
-		if mt == nil then return false end
-		if tostring(mt) == super then return true end
-		mt = getmetatable(mt);
-	end
-end
-
-function ctype(obj, strict)
-	if(strict == nil) then
-		strict = true;
-	end
-	local t = type(obj)
-	if t == 'table' then
-		local mt = getmetatable(obj)
-		if mt and mt.__type then
-			if type(mt.__type) == 'string' then
-				t = tostring(mt.__type)
-			else
-				pcall(function()
-					t = tostring(mt:__type())
-				end)
-			end
-		end
-	elseif t == 'userdata' then
-		if(obj.getClass) then
-			local str = obj:getClass():getName();
-			local i = str:find('%$');
-			if(i) then
-				t = str:sub(i + 1);
-			else
-				t = str;
-			end
-		else
-			t = tostring(obj):split(" ")[1];
-		end
-	elseif t == 'number' then
-		if strict then
-			t = math.floor(obj) == obj and 'integer' or 'float';
-		else
-			t = 'number';
-		end
-	end
-	return t
-end
-
-local function getTableTypeSignature(tbl, strict)
-	if(strict == nil) then
-		strict = false;
-	end
-	local signature = '';
-	local l = #tbl;
-	for i, v in ipairs(tbl) do
-		signature = signature .. ctype(v, strict) .. (i == l and '' or ',');
-	end
-	return signature;
-end
-
 local MeowClass = {};
-MeowCore = {};
+
+MeowCore = {
+	isctype = isctype,
+	typed = typed,
+	ccast = ccast,
+	cinstanceof = cinstanceof,
+	ctype = ctype,
+	switch = switch,
+	Typed = function(str)
+		local gets = luautils.split(str, ',');
+		local r = {};
+		for _,v in ipairs(gets) do
+			print(Typed[v:trim()]);
+			table.insert(r, Typed[v:trim()]);
+		end
+		return unpack(r);
+	end
+};
 
 local _required = {};
 
