9f8509babb2d764c115116e1a8eed409957ec14a
Woem <l.stoller@outlook.fr>
2021-10-11 13:06:56 +0200

    Update UILayout.lua

--- media/lua/client/UserInterface/Layouts/UILayout.lua
+++ media/lua/client/UserInterface/Layouts/UILayout.lua
@@ -1,44 +1,38 @@
 require "MeowCore";
 
-local typed, ctype = MeowCore.Typed("typed, ctype");
 local Vector2 = MeowCore.require("Shared/Math/Geometry/Vector2");
+local Dimension2 = MeowCore.require("Shared/Math/Geometry/Dimension2");
 
 local UILayout = MeowCore.class(
 	"Client/UserInterface/Layouts/UILayout", {
 		root = nil,
 		gap = Vector2:new(5, 5),
-		axe = 0,
 		expandChilds = false,
+		minimumChildWidth = 'auto',
 		fixedColumns = false,
 		reverse = false,
 		current = Vector2:new(0, 0),
-		childrens = {}
+		size = Dimension2:new(0, 0),
+		childrens = {},
+		isDirty = false
 	}
 );
 
-UILayout.AXE_X = 0;
-UILayout.AXE_Y = 1;
-UILayout.AXE_XY = 2;
-
-function UILayout:constructor(root, axe)
-	if(axe ~= nil) then
-		axe = typed('string', axe);
-	end
+function UILayout:constructor(root)
 	self.root = root;
-	self.axe = axe or self.axe;
 	self:calcInitialPos();
 end
 
 function UILayout:calcInitialPos()
 	if(self.reverse) then -- Reverse
-		if(self.axe == UILayout.AXE_X) then
-			self.current.x = self.root.width;
-			self.current.y = 0;
-		end
+		self.current.x = self.root.width;
+		self.current.y = 0;
 	else
 		self.current.x = 0;
 		self.current.y = 0;
 	end
+	self.size.w = 0;
+	self.size.h = 0;
 end
 
 function UILayout:recalcLayout()
@@ -46,11 +40,13 @@ function UILayout:recalcLayout()
 	for _, child in ipairs(self.childrens) do
 		self:calcPos(child);
 	end
+	self.size.w = self.size.w - self.gap.x;
+	self.isDirty = false;
 end
 
 function UILayout:calcPos(child)
 
-	if(self.fixedColumns ~= false and self.axe == UILayout.AXE_XY) then
+	if(self.fixedColumns ~= false) then
 		child:setWidth((
 			self.root.width / self.fixedColumns
 		) - (
@@ -58,8 +54,8 @@ function UILayout:calcPos(child)
 		));
 	end
 
-	if(self.expandChilds == true and self.axe == UILayout.AXE_X) then
-		child.minimumWidth = 100;
+	if(self.expandChilds == true) then
+		child.minimumWidth = self.minimumChildWidth == 'auto' and child.width or self.minimumChildWidth;
 		local childrenCount = #self.childrens;
 		child:setWidth((
 			self.root.width / childrenCount
@@ -70,47 +66,30 @@ function UILayout:calcPos(child)
 
 	if(self.reverse) then -- Reverse
 
-		if(self.axe == UILayout.AXE_XY) then
-			if(self.current.x - math.max(child.width, child.minimumWidth) < 0) then
-				self.current.x = self.root.width;
-				self.current.y = self.current.y + self.gap.y + child.height; -- TODO: replace by max last row height
-				self.root:setHeight(self.current.y + child.height);
-			end
-		end
-
-		if(self.axe == UILayout.AXE_X or self.axe == UILayout.AXE_XY) then
-			child:setX(self.current.x - math.max(child.width, child.minimumWidth));
-			child:setY(self.current.y);
-		end
+		child:setX(self.current.x - math.max(child.width, child.minimumWidth));
+		child:setY(self.current.y);
 
 	else	-- Normal
-
-		if(self.axe == UILayout.AXE_XY) then
-			if(self.current.x + math.max(child.width, child.minimumWidth) > self.root.width) then
-				self.current.x = 0;
-				self.current.y = self.current.y + self.gap.y + child.height; -- TODO: replace by max last row height
-				self.root:setHeight(self.current.y + child.height);
-			end
-		end
-
 		child:setX(self.current.x);
 		child:setY(self.current.y);
 	end
 
 	if(self.reverse) then -- Reverse
-		if(self.axe == UILayout.AXE_X or self.axe == UILayout.AXE_XY) then
-			self.current.x = child.x - self.gap.x;
-		end
+		self.current.x = child.x - self.gap.x;
 	else	-- Normal
-		if(self.axe == UILayout.AXE_X or self.axe == UILayout.AXE_XY) then
-			self.current.x = child.x + math.max(child.width, child.minimumWidth) + self.gap.x;
-		end
+		self.current.x = child.x + math.max(child.width, child.minimumWidth) + self.gap.x;
+	end
+
+	self.size.w = self.size.w + math.max(child.width, child.minimumWidth) + self.gap.x;
+
+	if(self.size.h < child:getHeight()) then
+		self.size.h = child:getHeight();
 	end
 
 end
 
 function UILayout:addChild(child)
-
+	self.isDirty = true;
 	self:calcPos(child);
 	self.root:addChild(child);
 	table.insert(self.childrens, child);
