01b9bcbf051e6d907eb561e4a67c943d6f1f00b8
Woem <l.stoller@outlook.fr>
2021-10-11 13:06:20 +0200

    Add round function

--- media/lua/shared/Math/Math.lua
+++ media/lua/shared/Math/Math.lua
@@ -3,3 +3,7 @@ math.clamp = function(number, nmin, nmax)
 	if(number > nmax) then number = nmax; end
 	return number;
 end
+
+math.round = function(x)
+  return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+end
