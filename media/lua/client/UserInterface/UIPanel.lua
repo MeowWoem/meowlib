8abc15e83acfc340ccaa72006f523e4ae41c2958
Woem <l.stoller@outlook.fr>
2021-10-11 13:07:23 +0200

    Update UIPanel.lua

--- media/lua/client/UserInterface/UIPanel.lua
+++ media/lua/client/UserInterface/UIPanel.lua
@@ -1,13 +1,19 @@
 require "MeowCore";
 require "ISUI/ISMouseDrag";
 
+local ISScrollBarMinimalist = require "UserInterface/ISScrollBarMinimalist"
+
 local UIPanel = MeowCore.derive("Client/UserInterface/UIPanel", "Client/UserInterface/UIComponent", {
 	moveWithMouse = false
 });
 
-
 function UIPanel:prerender()
 	UIPanel:super().prerender(self);
+	if(self.mouseOver and self.hscroll and not self.hscroll:getIsVisible()) then
+		self.hscroll:setVisible(true);
+	elseif(not self.mouseOver and self.hscroll and self.hscroll:getIsVisible()) then
+		self.hscroll:setVisible(false);
+	end
 	-- Code, Cred1, Cred2, Dialogue, Intro, Large, MainMenu1, MainMenu2, Massive, Medium, MediumNew,
 	-- NewLarge, NewMedium, NewSmall, Small
 	--self:drawText("UI PANEL", 0, 0, .5, .3, .3, 1, UIFont.Small);
@@ -22,7 +28,7 @@ function UIPanel:onMouseUp(x, y)
 	if(event ~= nil and event.preventDefault) then
 		return;
 	end
-    if not self.moveWithMouse then return event; end
+    if not self.moveWithMouse then return; end
     if not self:getIsVisible() then
         return;
     end
@@ -76,7 +82,7 @@ function UIPanel:onMouseMoveOutside(dx, dy)
 	if(event ~= nil and event.preventDefault) then
 		return;
 	end
-    if not self.moveWithMouse then return event; end
+    if not self.moveWithMouse then return end
 
     if self.moving then
         if self.parent then
@@ -96,7 +102,7 @@ function UIPanel:onMouseMove(dx, dy)
 	if(event ~= nil and event.preventDefault) then
 		return;
 	end
-    if not self.moveWithMouse then return event; end
+    if not self.moveWithMouse then return; end
 
     if self.moving then
         if self.parent then
@@ -110,4 +116,56 @@ function UIPanel:onMouseMove(dx, dy)
         --ISMouseDrag.dragView = self;
     end
 	return;
+end
+
+
+
+function UIPanel:addScrollBars(addHorizontal)
+	self.vscroll = ISScrollBarMinimalist:new(self, true);
+	self.vscroll:initialise();
+	self:addChild(self.vscroll);
+	self.vscroll:bringToTop();
+	if addHorizontal then
+		self.hscroll = ISScrollBarMinimalist:new(self, false)
+		self.hscroll:initialise()
+		self:addChild(self.hscroll)
+		self.hscroll:setVisible(false);
+		self.hscroll:bringToTop();
+	end
+end
+
+function UIPanel:setXScroll(x)
+	if self.javaObject == nil then
+		return;
+	end
+    if -x > self:getScrollWidth() - (self:getScrollAreaWidth()) then
+        x = -(self:getScrollWidth() - (self:getScrollAreaWidth()));
+    end
+    if -x < 0 then
+        x = 0;
+    end
+	self.javaObject:setXScroll(x);
+end
+
+function UIPanel:updateScrollbars()
+
+    if self.vscroll ~= nil then
+        self.vscroll:updatePos();
+    end
+
+    if self.hscroll ~= nil then
+        self.hscroll:updatePos();
+    end
+
+    local y = self.javaObject:getYScroll();
+
+    if -y > self:getScrollHeight() - (self:getScrollAreaHeight()) then
+        y = -(self:getScrollHeight() - (self:getScrollAreaHeight()));
+    end
+    if -y < 0 then
+        y = 0;
+    end
+
+    self.javaObject:setYScroll(y);
+
 end
